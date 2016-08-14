80bd68bb025d0b7813be80a92218bd35eede09b4
YuahP <yuah.build@gmail.com>
2016-08-14 23:14:47 +0900

    First Upload

---
+++ app/src/main/java/studio/ahope/project_ahope1/MainActivity.java
@@ -0,0 +1,13 @@
+package studio.ahope.project_ahope1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
