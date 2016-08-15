03b74592bb3731ca02208527c9c3f8a019e89004
Park Younggun <ygpark0922@gmail.com>
2016-08-16 00:24:41 +0900

    오 이거 된다 개쩔군

--- app/src/main/java/studio/ahope/project_ahope1/MainActivity.java
+++ app/src/main/java/studio/ahope/project_ahope1/MainActivity.java
@@ -1,13 +1,29 @@
 package studio.ahope.project_ahope1;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Drawable drawable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        getResource(R.drawable.testbg);
+        getWindow().setBackgroundDrawable(drawable);
+        drawable = null;
+    }
+
+    public void getResource(int dr){
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            drawable = ContextCompat.getDrawable(getApplicationContext(),dr);
+        }else{
+            drawable = getResources().getDrawable(dr);
+        }
     }
 }
